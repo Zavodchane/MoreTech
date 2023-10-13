116a47b4a1cc5f24bb3bd82286dd9a507af73b01
arcanegolem <dopeflipper@gmail.com>
2023-10-14 02:03:14 +0300

    Имплементирован BottomSheetScaffold вместо Surface, внутри тестовый контент `TODO: Добавить контент и фильтры`

--- app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayApp.kt
+++ app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayApp.kt
@@ -1,22 +1,44 @@
 package ru.zavodchane.moretech.presentation
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 import org.osmdroid.views.MapView
 import ru.zavodchane.moretech.data.atmMockList
 import ru.zavodchane.moretech.data.buildingMockList
 import ru.zavodchane.moretech.presentation.map.MapViewComposable
 import ru.zavodchane.moretech.ui.theme.MoreTechTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VTBBranchDisplayApp( mv : MapView ) {
    MoreTechTheme {
-      Surface(
-         modifier = Modifier.fillMaxSize(),
-         color = MaterialTheme.colorScheme.background
-      ) { MapViewComposable(buildings = buildingMockList, atms = atmMockList, mv = mv) }
+      val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+      val configuration = LocalConfiguration.current
+      val screenHeight = configuration.screenHeightDp.dp
+      BottomSheetScaffold(
+         scaffoldState = bottomSheetScaffoldState,
+         sheetContent = {
+            Column(
+               modifier = Modifier.height(screenHeight / 2).fillMaxWidth(),
+               verticalArrangement = Arrangement.Center,
+               horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+               Text("Test Content!")
+            }
+         }
+      ) {
+         MapViewComposable(buildings = buildingMockList, atms = atmMockList, mv = mv)
+      }
    }
 }
