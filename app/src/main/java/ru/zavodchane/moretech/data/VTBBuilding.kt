b62c7d731f9edd6860f58bc27484dfbcb19387ce
arcanegolem <dopeflipper@gmail.com>
2023-10-15 02:26:09 +0300

    Обновлены моки и дата класс отделения

--- app/src/main/java/ru/zavodchane/moretech/data/VTBBuilding.kt
+++ app/src/main/java/ru/zavodchane/moretech/data/VTBBuilding.kt
@@ -1,9 +1,11 @@
 package ru.zavodchane.moretech.data
 
+import android.util.Log
+
 data class VTBBuilding(
    val salePointName                   : String,                     // Наименование
    val address                         : String,                     // Адрес
-   val openHours                       : List<Map<String, String>>,  // Часы работы юр. лица
+   val openHours                       : List<Map<String, String?>>,  // Часы работы юр. лица
    val rko                             : Boolean,                     // Наличие РКО
    val openHoursIndividual             : List<Map<String, String>>,  // Часы работы физ. лица
    val officeType                      : String,                     // Открытый тип офиса (???)
@@ -15,21 +17,21 @@ data class VTBBuilding(
    var distance                        : Int = 0,                    // Дистанция до отделения от местоположения пользователя
    val kep                             : Boolean,                   // Признак выдачи КЭП
    val myBranch                        : Boolean,                    // Признак "Мое отделение"
-   var ratings                         : Float,
-   var deposit_boxes                   : Boolean,
-   var biometric_data_collection       : Boolean,
-   var wifi                            : Boolean,
-   var deposit_in_rubles               : Boolean,
-   var deposit_in_foreign_currency     : Boolean,
-   var deposits_in_precious_metals     : Boolean,
-   var transactions_with_non_cash      : Boolean,
-   var cash_transactions               : Boolean,
-   var operations_with_precious_metals : Boolean,
-   var workload                        : List<Map<String, String>>,
-   var workload_online                 : Float = 0f,
-   var costumers                       : Int,
-   var workers                         : Int,
-   var koef_avg_time                   : Float
+   val ratings                         : Float,
+   val deposit_boxes                   : Boolean,
+   val biometric_data_collection       : Boolean,
+   val wifi                            : Boolean,
+   val deposit_in_rubles               : Boolean,
+   val deposit_in_foreign_currency     : Boolean,
+   val deposits_in_precious_metals     : Boolean,
+   val transactions_with_non_cash      : Boolean,
+   val cash_transactions               : Boolean,
+   val operations_with_precious_metals : Boolean,
+//   var workload                        : List<Map<String, String>>,
+   val workload_online                 : Float = 0f,
+   val costumers                       : Int,
+   val workers                         : Int,
+   val koef_avg_time                   : Float
 ) {
    fun isMatchingSearchQuery(query: String, filters: ClientFilters) : Boolean {
       val queryList = query.split(" ")
@@ -40,17 +42,12 @@ data class VTBBuilding(
 
       val regexPattern = "^$newRegex.*$".toRegex(setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE, RegexOption.DOT_MATCHES_ALL, RegexOption.UNIX_LINES))
       val branchParamsString = "$salePointName + $address + $metroStation"
-      return regexPattern.containsMatchIn(branchParamsString) and isMatchingFilters(filters)
+      Log.i("RegexRes", "${regexPattern.containsMatchIn(branchParamsString)}")
+      Log.i("FilterRes", "${isMatchingFilters(filters)}")
+      return regexPattern.containsMatchIn(branchParamsString) && isMatchingFilters(filters)
    }
 
    private fun isMatchingFilters(filters: ClientFilters) : Boolean {
-      if (filters.rko)                          { if (!rko) return false }
-      if (filters.hasRamp)                      { if (!hasRamp) return false }
-      if (filters.depositBoxes)                 { if (!deposit_boxes) return false }
-      if (filters.depositInRubles)              { if (!deposit_in_rubles) return false }
-      if (filters.depositInForeignCurrency)     { if (!deposit_in_foreign_currency) return false }
-      if (filters.depositInPreciousMetals)      { if (!deposits_in_precious_metals) return false }
-      if (filters.operationsWithPreciousMetals) { if (!operations_with_precious_metals) return false }
       return true
    }
 }
