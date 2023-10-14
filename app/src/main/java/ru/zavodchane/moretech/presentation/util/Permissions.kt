7af4cf5e0e4f75483cdccc63ab6ef4e38ed002d0
arcanegolem <dopeflipper@gmail.com>
2023-10-14 19:23:03 +0300

    Возвращаемый тип изменен на Array с List

--- app/src/main/java/ru/zavodchane/moretech/presentation/util/Permissions.kt
+++ app/src/main/java/ru/zavodchane/moretech/presentation/util/Permissions.kt
@@ -2,8 +2,8 @@ package ru.zavodchane.moretech.presentation.util
 
 import android.Manifest
 
-fun permissions() : List<String> {
-   return listOf(
+fun permissions() : Array<String> {
+   return arrayOf(
       Manifest.permission.ACCESS_FINE_LOCATION,
       Manifest.permission.ACCESS_COARSE_LOCATION
    )
