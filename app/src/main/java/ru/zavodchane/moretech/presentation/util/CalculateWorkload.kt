0db4202cc39993669c13694a5f59b4580b54eed8
arcanegolem <dopeflipper@gmail.com>
2023-10-14 22:56:47 +0300

    Рефактор

--- app/src/main/java/ru/zavodchane/moretech/presentation/util/CalculateWorkload.kt
+++ app/src/main/java/ru/zavodchane/moretech/presentation/util/CalculateWorkload.kt
@@ -4,7 +4,7 @@ private const val MEDIAN_SERVICE_TIME_MIN = 5.5F
 fun calculateWorkload(
    workers : Int,
    clients : Int,
-   currentEstimatedServiceTime : Int
+   currentEstimatedServiceTimeMin : Int
 ) : Float {
-   return (clients * currentEstimatedServiceTime) / (workers * MEDIAN_SERVICE_TIME_MIN)
+   return (clients * currentEstimatedServiceTimeMin) / (workers * MEDIAN_SERVICE_TIME_MIN)
 }
