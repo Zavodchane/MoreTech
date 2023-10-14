602d544dc6a5a32c8526c2be66ba37a862247ff8
arcanegolem <dopeflipper@gmail.com>
2023-10-14 19:23:26 +0300

    Работа с локацией и разрешениями

--- app/src/main/java/ru/zavodchane/moretech/MainActivity.kt
+++ app/src/main/java/ru/zavodchane/moretech/MainActivity.kt
@@ -1,9 +1,26 @@
 package ru.zavodchane.moretech
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.IntentSender
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationAvailability
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer
 import org.osmdroid.views.MapView
 import ru.zavodchane.moretech.presentation.VTBBranchDisplayApp
@@ -11,14 +28,36 @@ import ru.zavodchane.moretech.presentation.VTBBranchDisplayViewModel
 import ru.zavodchane.moretech.presentation.map.clustering.setupATMMarkerClusterer
 import ru.zavodchane.moretech.presentation.map.clustering.setupBuildingMarkerClusterer
 import ru.zavodchane.moretech.presentation.map.mapview.setupMapView
+import ru.zavodchane.moretech.presentation.util.permissions
 
 lateinit var OSMMapView : MapView
 lateinit var buildingRadiusMarkerClusterer: RadiusMarkerClusterer
 lateinit var atmRadiusMarkerClusterer: RadiusMarkerClusterer
+lateinit var fusedLocationClient: FusedLocationProviderClient
+var currentLocationFlow: MutableStateFlow<Location?> = MutableStateFlow(null)
 
 class MainActivity : ComponentActivity() {
+   @SuppressLint("MissingPermission") // Запуск зависимой функции происходит только после проверки разрешений
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
+
+      fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+      val locationPermissionRequest = registerForActivityResult(
+         ActivityResultContracts.RequestMultiplePermissions()
+      ) { permissions ->
+         when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+               setupLocation()
+               return@registerForActivityResult
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+               setupLocation()
+               return@registerForActivityResult
+            } else -> { /* TODO */ }
+         }
+      }
+      locationPermissionRequest.launch(permissions())
 
       setupMapView(this, packageName)
       setupBuildingMarkerClusterer(this)
@@ -27,6 +66,45 @@ class MainActivity : ComponentActivity() {
       setContent {
          val viewModel = viewModel<VTBBranchDisplayViewModel>()
          VTBBranchDisplayApp(vm = viewModel, mv = OSMMapView)
+      }
+   }
+
+   @SuppressLint("MissingPermission") // Запуск функции происходит только после проверки разрешений
+   private fun setupLocation() {
+      val locationRequest = LocationRequest.Builder(
+         Priority.PRIORITY_HIGH_ACCURACY,
+         10000
+      ).build()
+      val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+      val client = LocationServices.getSettingsClient(this)
+      val task = client.checkLocationSettings(builder.build())
+
+      task.addOnSuccessListener {
+         val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+               super.onLocationResult(locationResult)
+               locationResult ?: return
+               for (location in locationResult.locations) {
+                  currentLocationFlow.value = location
+                  Log.i("CurrentLocation", "${currentLocationFlow.value?.latitude} -- ${currentLocationFlow.value?.longitude}")
+               }
+            }
+
+            override fun onLocationAvailability(locationAvailability: LocationAvailability) {
+               super.onLocationAvailability(locationAvailability)
+               Log.i("LocationAvailability", "${locationAvailability.isLocationAvailable}")
+            }
+         }
+         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+         Log.i("LocationSettings", "Granted")
+      }
+
+      task.addOnFailureListener { exception ->
+         Log.i("LocationSettings", "Not granted! Showing dialog...")
+         if (exception is ResolvableApiException){
+            try { exception.startResolutionForResult(this@MainActivity, 0x1)
+            } catch (sendEx: IntentSender.SendIntentException) {/* Ignore the error. */}
+         }
       }
    }
 
