969675d00fedd5193ad897016e9ad7608f7e0d04
arcanegolem <dopeflipper@gmail.com>
2023-10-14 03:47:14 +0300

    Брендовые цвета ВТБ

--- app/src/main/java/ru/zavodchane/moretech/ui/theme/Color.kt
+++ app/src/main/java/ru/zavodchane/moretech/ui/theme/Color.kt
@@ -9,3 +9,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Pantone228C   = Color(0xFF002882)
+val Pantone228C80 = Color(0xFF33539B)
+val Pantone228C60 = Color(0xFF667EB4)
+val Pantone228C40 = Color(0xFF99A9CD)
+val Pantone228C20 = Color(0xFFCCD4E6)
+
+val ProcessCyan   = Color(0xFF00AAFF)
+val ProcessCyan40 = Color(0xFF9BDDFC)
+val ProcessCyan20 = Color(0xFFE5F6FF)
+
+val ProcessBlack  = Color(0xFF000000)
+
+val CoolGrey10    = Color(0xFF58595B)
+val CoolGrey8     = Color(0xFF878787)
+val CoolGrey6     = Color(0xFFC5C7C8)
