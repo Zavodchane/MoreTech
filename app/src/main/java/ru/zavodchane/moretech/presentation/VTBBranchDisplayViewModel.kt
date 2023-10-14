a0ef56b86d584b240cef9623587a7f5f944a232a
arcanegolem <dopeflipper@gmail.com>
2023-10-14 19:24:23 +0300

    Убрана старая функция работы с геолокацией

--- app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayViewModel.kt
+++ app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayViewModel.kt
@@ -1,35 +1,20 @@
 package ru.zavodchane.moretech.presentation
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.location.Location
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.osmdroid.util.GeoPoint
 import ru.zavodchane.moretech.OSMMapView
+import ru.zavodchane.moretech.currentLocationFlow
 import ru.zavodchane.moretech.data.ClientType
 
 class VTBBranchDisplayViewModel () : ViewModel() {
-   private val _currentLocation = MutableStateFlow<Location?>(null)
+   private val _currentLocation = currentLocationFlow
    val currentLocation = _currentLocation.asStateFlow()
 
    private val _clientType = MutableStateFlow(ClientType.PHYSICAL_ENTITY)
    val clientType = _clientType.asStateFlow()
-
-   @SuppressLint("MissingPermission")
-   fun getSingleRequestCurrentLocation(ctx : Context) {
-      val fusedLocationProvider = LocationServices.getFusedLocationProviderClient(ctx)
-
-      fusedLocationProvider.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
-         .addOnSuccessListener { receivedLocation ->
-            _currentLocation.value = receivedLocation
-            Log.w("CurrentLocation", "Current location received ${currentLocation.value?.latitude} -- ${currentLocation.value?.longitude}")
-         }
-   }
 
    fun changeClientType(ct: ClientType) {
       _clientType.value = ct
