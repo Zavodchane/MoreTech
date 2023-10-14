7b261da0fae4071e8844cb76bab8e606ccdee387
LIvanoff <drfrily@gmail.com>
2023-10-14 03:52:08 +0300

    VTBBranchDisplayApp.kt, тестовый вывод BottomSheet

--- app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayApp.kt
+++ app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayApp.kt
@@ -1,16 +1,23 @@
 package ru.zavodchane.moretech.presentation
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import org.osmdroid.views.MapView
@@ -29,12 +36,48 @@ fun VTBBranchDisplayApp( mv : MapView ) {
       BottomSheetScaffold(
          scaffoldState = bottomSheetScaffoldState,
          sheetContent = {
-            Column(
-               modifier = Modifier.height(screenHeight / 2).fillMaxWidth(),
-               verticalArrangement = Arrangement.Center,
+            LazyColumn(
+               modifier = Modifier
+                  .height(screenHeight * 2 / 3)
+                  .fillMaxWidth(),
+//               verticalArrangement = Arrangement.SpaceAround,
                horizontalAlignment = Alignment.CenterHorizontally
-            ) { Text("Test Content!") }
+            ) {
+               val listOfVTB = buildingMockList
+               listOfVTB.forEach{
+                  item { Card(
+                     modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp),
+                     shape = RoundedCornerShape(5.dp),
+                     elevation = CardDefaults.elevatedCardElevation(5.dp),
+                     colors = CardDefaults.cardColors(containerColor = Color.Cyan)
+                  ) {
+                     Row(
+                        modifier = Modifier.background(Color.Yellow)
+                     ) {
+                        Text(text = it.salePointName)
+                     }
+                     Column() {
+                        Text(text = it.address)
+                        Text(text = it.status)
+                     }
+
+                  }
+
+
+                  }
+//                  Row(
+//                     modifier = Modifier.background(Color.Blue ).size(dp),
+//                     horizontalArrangement = Arrangement.SpaceAround
+//                  ){
+//                     Text(text = it)
+//                  }
+               }
+            }
          }
-      ) { MapViewComposable(buildings = buildingMockList, atms = atmMockList, mv = mv) }
+      ) {
+         MapViewComposable(buildings = buildingMockList, atms = atmMockList, mv = mv)
+      }
    }
 }
