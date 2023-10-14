fa8d7f79aefbfcc06028eeb7fe8d04683c984725
arcanegolem <dopeflipper@gmail.com>
2023-10-14 16:47:28 +0300

    Апдейт UI

--- app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayApp.kt
+++ app/src/main/java/ru/zavodchane/moretech/presentation/VTBBranchDisplayApp.kt
@@ -10,40 +10,47 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import org.osmdroid.views.MapView
 import ru.zavodchane.moretech.data.atmMockList
 import ru.zavodchane.moretech.data.buildingMockList
+import ru.zavodchane.moretech.presentation.bottomsheetcontent.ClientTypeSwitch
 import ru.zavodchane.moretech.presentation.map.MapViewComposable
 import ru.zavodchane.moretech.presentation.util.permissions
 import ru.zavodchane.moretech.ui.theme.MoreTechTheme
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
-fun VTBBranchDisplayApp( vm : ViewModel, mv : MapView ) {
+fun VTBBranchDisplayApp( vm : VTBBranchDisplayViewModel, mv : MapView ) {
    val permissionState = rememberMultiplePermissionsState(permissions = permissions())
    if (!permissionState.allPermissionsGranted) { SideEffect { permissionState.launchMultiplePermissionRequest() } }
+
+   val currentClientTypeState = vm.clientType.collectAsState()
 
    MoreTechTheme {
       val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
       val configuration = LocalConfiguration.current
       val screenHeight = configuration.screenHeightDp.dp
       BottomSheetScaffold(
+         sheetPeekHeight = 50.dp,
          scaffoldState = bottomSheetScaffoldState,
          sheetContent = {
             Column(
                modifier = Modifier
                   .height(screenHeight / 2)
                   .fillMaxWidth(),
-               verticalArrangement = Arrangement.Center,
+               verticalArrangement = Arrangement.Top,
                horizontalAlignment = Alignment.CenterHorizontally
-            ) { Text("Test Content!") }
+            ) {
+               ClientTypeSwitch(onClientTypeChange = vm::changeClientType, currentClientType = currentClientTypeState.value)
+               Text("Test Content!")
+            }
          }
       ) { MapViewComposable(buildings = buildingMockList, atms = atmMockList, mv = mv) }
    }
---
+++ app/src/main/java/ru/zavodchane/moretech/presentation/bottomsheetcontent/ParametersContent.kt
@@ -0,0 +1,76 @@
+package ru.zavodchane.moretech.presentation.bottomsheetcontent
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import androidx.compose.ui.zIndex
+import ru.zavodchane.moretech.data.ClientType
+
+@Composable
+fun ClientTypeSwitch(onClientTypeChange : (ClientType) -> Unit, currentClientType: ClientType) {
+   var rowSize by remember { mutableStateOf(Size.Zero) }
+
+   Row(
+      modifier = Modifier
+         .fillMaxWidth()
+         .zIndex(2f)
+         .background(Color.Transparent)
+         .padding(horizontal = 10.dp, vertical = 5.dp)
+         .border(1.dp, Color.Black, RoundedCornerShape(5.dp))
+         .onGloballyPositioned { coordinates -> rowSize = coordinates.size.toSize() },
+      horizontalArrangement = Arrangement.SpaceBetween
+   ) {
+      val interactionSource = remember { MutableInteractionSource() }
+      Text(
+         modifier = Modifier
+            .clickable(interactionSource = interactionSource, indication = null) {
+               onClientTypeChange(ClientType.PHYSICAL_ENTITY)
+            }
+            .width(with(LocalDensity.current) { rowSize.width.toDp() / 2 - (2.5).dp })
+            .background(
+               color = if (currentClientType == ClientType.PHYSICAL_ENTITY) Color.LightGray else Color.Transparent,
+               shape = RoundedCornerShape(5.dp)
+            )
+            .padding(vertical = (2.5).dp),
+         text = "Физлицо",
+         textAlign = TextAlign.Center
+      )
+      Spacer(modifier = Modifier.width(5.dp))
+      Text(
+         modifier = Modifier
+            .clickable(interactionSource = interactionSource, indication = null) {
+               onClientTypeChange(ClientType.LEGAL_ENTITY)
+            }
+            .width(with(LocalDensity.current) { rowSize.width.toDp() / 2 - (2.5).dp })
+            .background(
+               color = if (currentClientType == ClientType.LEGAL_ENTITY) Color.LightGray else Color.Transparent,
+               shape = RoundedCornerShape(5.dp)
+            )
+            .padding(vertical = (2.5).dp),
+         text = "Юрлицо",
+         textAlign = TextAlign.Center
+      )
+   }
+}
