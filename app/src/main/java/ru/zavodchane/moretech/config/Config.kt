ab4e9b93c1c430e1cc855933fb047f6d838080cf
arcanegolem <dopeflipper@gmail.com>
2023-10-14 22:15:44 +0300

    Апдейт конфига

---
+++ app/src/main/java/ru/zavodchane/moretech/config/Config.kt
@@ -0,0 +1,15 @@
+package ru.zavodchane.moretech.config
+
+import org.osmdroid.util.GeoPoint
+
+// Location
+const val LOCATION_RETRIEVAL_INTERVAL = 1000L
+
+// Map
+const val INITIAL_ZOOM = 15.0
+const val INITIAL_ANIM_SPEED = 1000L
+
+val INITIAL_GEO_POINT = GeoPoint(55.762936, 37.628845)
+
+// Request codes
+const val RESOLUTION_REQUEST_CODE = 0x1
--- app/src/main/java/ru/zavodchane/moretech/config/MapConfigValues.kt
+++
@@ -1,8 +0,0 @@
-package ru.zavodchane.moretech.config
-
-import org.osmdroid.util.GeoPoint
-
-const val INITIAL_ZOOM = 15.0
-const val INITIAL_ANIM_SPEED = 1000L
-
-val INITIAL_GEO_POINT = GeoPoint(55.762936, 37.628845)
