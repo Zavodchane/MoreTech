1a8b00238d3e3bfa3dae3dfe0ed8b385a5dbbdf8
arcanegolem <dopeflipper@gmail.com>
2023-10-15 06:36:20 +0300

    GSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
 
    implementation("com.google.android.gms:play-services-location:21.0.1")
 
+   implementation("com.google.code.gson:gson:2.9.0")
+
    implementation("org.osmdroid:osmdroid-android:6.1.13")
    implementation("com.github.MKergall:osmbonuspack:6.9.0")
 }
